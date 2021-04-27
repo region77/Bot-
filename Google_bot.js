8aaff28610647ad48324fd82e8af989c2c09f6c8
region77 <82330883+region77@users.noreply.github.com>
2021-04-27 11:13:34 +0300

    27/04/21
    переключение по страницам

--- Google_bot.js
+++ Google_bot.js
@@ -8,27 +8,52 @@
 // @icon         
 // @grant        none
 // ==/UserScript==
-let keywords = ["вывод произвольных полей wordpress", "10 самых популярных шрифтов от Google", "отключение редакций и ревизий в WordPress"];
+
+let keywords = ["вывод произвольных полей wordpress", "pods wordpress", "10 самых популярных шрифтов от Google", "Отключение редакций и ревизий в WordPress"];
 
 let btnK = document.getElementsByName('btnK')[0];
 let links = document.links;
-let keyword = keywords[getRandom(0, keywords.length)];
+let keyword = keywords[getRandom(0,keywords.length)];
+let googleInput = document.getElementsByName('q')[0];
+let i = 0;
 
 
 if(btnK !== undefined){
-    document.getElementsByName('q')[0].value = keyword;
-    document.getElementsByName('btnK')[0].click();
-}else{
-    for (let i=0; i<links.length; i++){
-        if (links[i].href.indexOf('napli.ru')!=-1){
-            let link = links[i];
-            console.log("Нашел фразу" + link);
-            link.click();
-            break;
-         }
-     }
- }
+	let timerId = setInterval(()=> {
+		googleInput.value += keyword[i];
+		i++;
+		if(i == keyword.length) {
+			clearInterval(timerId);
+			btnK.click();
+		}
+	}, 1000);
 
-function getRandom(min, max) {
- return Math.floor(Math.random()*(max-min)+min);
+
+}else{
+	let nextGooglePage = true;
+	for(let i=0; i<links.length; i++) {
+		if(links[i].href.indexOf('napli.ru')!=-1) {
+			let link = links[i];
+			nextGooglePage = false;
+			console.log("Нашел фразу" + link);
+			setTimeout(()=>{
+				link.click();}
+					   ,getRandom(1000,4500));
+			break;
+		}
+	}
+	if(document.querySelector('.YyVfkd').innerText == "5") {
+		nextGooglePage = false;
+		location.href = "https://www.google.com/";
+	}
+
+	if(document.querySelector('.YyVfkd').innerText !== "5") {
+				setTimeout(()=>{
+			pnnext.click();}
+				   ,getRandom(3000,5000));
+	}
+}
+
+function getRandom(min,max) {
+	return Math.floor(Math.random()*(max-min)+min);
 }
